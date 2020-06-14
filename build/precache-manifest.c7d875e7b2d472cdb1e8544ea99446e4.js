29bfe918963dbef796cf6d2b7e4b9f697cabb4cd
Jasper Jiao <jz630032@dal.ca>
2020-06-14 19:14:22 -0300

    add server.js

---
+++ build/precache-manifest.c7d875e7b2d472cdb1e8544ea99446e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "686386dd95825810a44600631af59d20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41a5d95d58ea3aa4495e",
+    "url": "/static/css/main.3d2e6400.chunk.css"
+  },
+  {
+    "revision": "74c2abb1c0def9c884c6",
+    "url": "/static/js/2.5347c817.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5347c817.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41a5d95d58ea3aa4495e",
+    "url": "/static/js/main.18d762e3.chunk.js"
+  },
+  {
+    "revision": "6ff7f8013853e0d57514",
+    "url": "/static/js/runtime-main.d48be983.js"
+  },
+  {
+    "revision": "7db3072d37d751753271649ca8571773",
+    "url": "/static/media/image1.7db3072d.png"
+  }
+]);
